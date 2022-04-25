c3073c946195b827ec09010c43315e8fdabc3c9c
Benjamin Woosley <benjaminwoosley@gmail.com>
2022-04-25 13:50:48 -0400

    Design Underground System

---
+++ 1396.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <utility>
+
+template <typename T>
+void print_vector(std::vector<T> vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+class UndergroundSystem
+{
+public:
+    std::unordered_map<int, std::pair<std::string, int> > checkInMap;
+    std::unordered_map<std::string, std::pair<double, int> > routeMap;
+    UndergroundSystem()
+    {
+    }
+
+    void checkIn(int id, std::string stationName, int t)
+    {
+        checkInMap[id] = {stationName, t};
+    }
+
+    void checkOut(int id, std::string stationName, int t)
+    {
+        auto checkIn = checkInMap[id];
+        checkInMap.erase(id); // remove after using it which will not make HashTable big
+
+        std::string routeName = checkIn.first + "_" + stationName;
+        routeMap[routeName].first += t - checkIn.second;
+        routeMap[routeName].second += 1;
+    }
+
+    double getAverageTime(std::string startStation, std::string endStation)
+    {
+        std::string routeName = startStation + "_" + endStation;
+        auto& route = routeMap[routeName];
+        return route.first / route.second;
+        return -1.0;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    UndergroundSystem* undergroundSystem = new UndergroundSystem();
+    undergroundSystem->checkIn(45, "Leyton", 3);
+    undergroundSystem->checkIn(32, "Paradise", 8);
+    undergroundSystem->checkIn(27, "Leyton", 10);
+    undergroundSystem->checkOut(45, "Waterloo", 15);  // Customer 45 "Leyton" -> "Waterloo" in 15-3 = 12
+    undergroundSystem->checkOut(27, "Waterloo", 20);  // Customer 27 "Leyton" -> "Waterloo" in 20-10 = 10
+    undergroundSystem->checkOut(32, "Cambridge", 22); // Customer 32 "Paradise" -> "Cambridge" in 22-8 = 14
+    undergroundSystem->getAverageTime("Paradise", "Cambridge"); // return 14.00000. One trip "Paradise" -> "Cambridge", (14) / 1 = 14
+    undergroundSystem->getAverageTime("Leyton", "Waterloo");    // return 11.00000. Two trips "Leyton" -> "Waterloo", (10 + 12) / 2 = 11
+    undergroundSystem->checkIn(10, "Leyton", 24);
+    undergroundSystem->getAverageTime("Leyton", "Waterloo");    // return 11.00000
+    undergroundSystem->checkOut(10, "Waterloo", 38);  // Customer 10 "Leyton" -> "Waterloo" in 38-24 = 14
+    undergroundSystem->getAverageTime("Leyton", "Waterloo");    // return 12.00000. Three trips "Leyton" -> "Waterloo", (10 + 12 + 14) / 3 = 12
+    return 0;
+}
